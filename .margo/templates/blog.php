41f2043547a4530d5062431fa624de6b5021da77
Jonathan Stark <jstark@jonathanstark.com>
2012-05-06 13:50:44 -0700

    added basic css styles to blog template

--- .margo/templates/blog.php
+++ .margo/templates/blog.php
@@ -7,20 +7,49 @@
         <link rel="alternate" type="application/rss+xml" title="<?php echo $blog->title ?>" href="<?php echo $blog->url ?>/rss">
         <link rel="service.post" type="application/atom+xml" title="<?php echo $blog->title ?>" href="<?php echo $blog->url ?>/atom">
         <style type="text/css" media="screen">
+            html {
+                background-color: #333;
+            }
             body {
                 font: 100%/1.5em Helvetica, sans-serif;
+                margin: 0;
+                padding: 0;
                 text-align: center;
             }
+            a {
+                color: darkmagenta;
+                font-weight: bold;
+                text-decoration: none;
+            }
+            a:hover {
+                color: white;
+                background-color: darkmagenta;
+            }
             #wrapper {
-                max-width: 800px;
+                background-color: #ccc;
+                max-width: 37.5em;
                 margin: 0 auto;
                 text-align: left;
+            }
+            #header, #content, #footer {
+                padding: 1em 2em;
+            }
+            #header {
+                background-color: #fff;
+            }
+            #content {
+                background-color: #eee;
+                min-height: 26.5em;
+            }
+            #footer {
+                background-color: #ddd;
+                text-align: center;
             }
         </style>
     </head>
     <body>
         <div id="wrapper">
-            <id="header">
+            <div id="header">
                 <h1><a href="<?php echo $blog->url ?>"><?php echo $blog->title ?></a></h1>
                 <p><?php echo $blog->description ?></p>
             </div>
