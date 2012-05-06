a6f736a165161118620a3170eb55caed84878996
Jonathan Stark <jstark@jonathanstark.com>
2012-05-06 16:39:12 -0400

    added som styles

--- .margo/templates/blog.php
+++ .margo/templates/blog.php
@@ -6,15 +6,32 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="alternate" type="application/rss+xml" title="<?php echo $blog->title ?>" href="<?php echo $blog->url ?>/rss">
         <link rel="service.post" type="application/atom+xml" title="<?php echo $blog->title ?>" href="<?php echo $blog->url ?>/atom">
-        <link rel="stylesheet" href="<?php echo $blog->css ?>" type="text/css" media="screen" title="no title" charset="utf-8">
+        <style type="text/css" media="screen">
+            body {
+                font: 100%/1.5em Helvetica, sans-serif;
+                text-align: center;
+            }
+            #wrapper {
+                max-width: 800px;
+                margin: 0 auto;
+                text-align: left;
+            }
+        </style>
     </head>
     <body>
-        <header>
-            <h1><a href="<?php echo $blog->url ?>"><?php echo $blog->title ?></a></h1>
-            <p><?php echo $blog->description ?></p>
-        </header>
-        <div class="section">
-            <?php echo $body ?>
+        <div id="wrapper">
+            <id="header">
+                <h1><a href="<?php echo $blog->url ?>"><?php echo $blog->title ?></a></h1>
+                <p><?php echo $blog->description ?></p>
+            </div>
+            <div id="content">
+                <div class="section">
+                    <?php echo $body ?>
+                </div>
+            </div>
+            <div id="footer">
+                &copy; Yada yada yada
+            </div>
         </div>
     </body>
 </html>
